dfdb5406fd118dd6d27b9d0ec9b7afb601ca8c7a
Harsha_784 <98533767+harshavardhan784@users.noreply.github.com>
2025-01-19 12:39:53 +0530

    Add files via upload

--- final_main.py
+++ final_main.py
@@ -561,7 +561,7 @@ def perform_semantic_search(session, user_id, rank=100, threshold=0.5):
         print("here5")
         # Step 6: Perform semantic search and combine results
         session.sql(f"""
-            CREATE OR REPLACE TABLE augment_table AS
+            CREATE OR REPLACE TABLE AUGMENT_TABLE AS
             WITH cross_product AS (
                 SELECT 
                     p.CATEGORY_1,
@@ -571,7 +571,7 @@ def perform_semantic_search(session, user_id, rank=100, threshold=0.5):
                     p.HIGHLIGHTS,
                     p.IMAGE_LINKS,
                     p.MRP,
-                    p.PRODUCT_ID,
+                    UNIQUE(p.PRODUCT_ID),
                     p.PRODUCT_RATING,
                     p.SELLER_NAME,
                     p.SELLER_RATING,
